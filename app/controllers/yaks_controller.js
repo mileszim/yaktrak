92ffe5afbcc63c14808249a5efee5644753e429e
Miles Zimmerman <miles@zserver.org>
2014-10-16 07:04:52 -0700

    fuck yeah

--- app/controllers/yaks_controller.js
+++ app/controllers/yaks_controller.js
@@ -5,7 +5,7 @@ module.exports = {
 	
 	index: function(req, res) {
 		Yak.find({})
-		.limit(1)
+		.limit(10)
 		.sort('-timestamp')
 		.exec(function(err, yaks) {
 			res.render('yaks/index', { yaks: yaks });
--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	db: 'mongodb://localhost/yaktrak',
 	
 	// School to fetch yaks from
-	school: 'University of Florida',
+	school: 'Chapman University',
 	
 	// Geocoding API key
 	geocode_key: 'AIzaSyAC02Zv19RtxSucTYfvROHpxewxEeuwSXs'
